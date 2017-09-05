dd08b89830ecda4c267209a143f09de9b3e3c34a
mblaki <mblaki69@gmail.com>
2017-09-05 17:07:24 +0200

    sdf

--- index.js
+++ index.js
@@ -9,16 +9,15 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+      io.emit('chat message', msg);
   });
     
-  socket.on('joinGame', function(username){
+    socket.on('joinGame', function(username){
       console.log("Player: " + username + " has connected");
-      socket.on('joinGame', function(username){
       io.emit('joinGame', username);          
-      });
 });
 });
+
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
