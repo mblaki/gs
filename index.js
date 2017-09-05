147d5a384de084db3650b188e4206a50b939ada0
mblaki <mblaki69@gmail.com>
2017-09-05 13:28:50 +0200

    fff

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 app.get('/', function(req, res){
-    res.sendFile(__dirname + 'www/index.html');
+    res.sendFile(__dirname + '/www/index.html');
 });
 io.on('connection', function(socket){
     console.log('user has  connected');
