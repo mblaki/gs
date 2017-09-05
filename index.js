8fa85041ec446672d5cd176ddfc6259d7ff4a43a
mblaki <mblaki69@gmail.com>
2017-09-05 13:03:56 +0200

    chat

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
-
+var io = require('socket.io')(http);
 app.get('/', function(req, res){
     res.sendFiel(__dirname + '/index.html');
+});
+io.on('connection', function(socket){
+    console.log('user has  connected');
+    socket.on("disconnect", function(){
+        console.log("user disconnected");
+    });
 });
 
 http.listen(8000, function(){
