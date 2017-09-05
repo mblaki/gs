ee83a5e0117de9ba93030f25f8da4341af2409fd
mblaki <mblaki69@gmail.com>
2017-09-05 14:28:04 +0200

    chanbe

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/www/index.html');
-});
-io.on('connection', function(socket){
-    console.log('user has  connected');
-    socket.on('chat message'), function(msg){
-        io.emit('chat message', msg);
-    }
-    socket.on("disconnect", function(){
-        console.log("user disconnected");
-    });
+  res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(8000, function(){
-    console.log('listening on 8000 brah');
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
