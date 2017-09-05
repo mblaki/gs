55ff799339ed1758dc8801d8629c563def5f5d00
mblaki <mblaki69@gmail.com>
2017-09-05 17:59:59 +0200

    sdf

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3003;
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-    console.log('user has  connected');
-    socket.on('chat message'), function(msg){
-        io.emit('chat message', msg);
-    }
-    socket.on("disconnect", function(){
-        console.log("user disconnected");
-    });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(port, function(){
-  console.log('listening on :' + port);
+  console.log('listening on *:' + port);
 });
