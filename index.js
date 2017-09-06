1358f93b4277447a9e0088a9653f4023e75018c5
mblaki <mblaki69@gmail.com>
2017-09-06 19:41:30 +0200

    sdfd

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 8020;
-//client[0] is the server GUIfor the Instructor, all other clients are Players
+//client["Instructor"] is the webpage for the Instructor, all other clients are Players
 var clients=[];
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/www/index.html');
 });
 io.on('connection', function(socket){
-    console.log("clients[] length= " + clients.length);
+    console.log("clients[] length= " + Object.keys(clients).length);
         socket.on('instructor join', function(){
             console.log("the instructor has started the simulation");
             clients["Instructor"]= socket;
@@ -21,10 +21,7 @@ io.on('connection', function(socket){
         });
     
     socket.on('chat message', function(msg, name){
-    console.log(name+ " sent a chat msg, bitch");
-    if ( name == "Instructor") {
-        clients[name].emit('chat message', msg, name);
-    } else {
+        console.log(name+ " sent a chat msg, bitch");
         clients["Instructor"].emit('chat message', msg, name);
         clients[name].emit('chat message', msg, name);
     }
