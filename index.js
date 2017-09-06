497fe548d65e0fa33447abde273035c4672d5427
mblaki <mblaki69@gmail.com>
2017-09-06 14:45:11 +0200

    df

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ io.on('connection', function(socket){
     clients.push(socket);
   socket.on('chat message', function(msg){
     console.log("chat msg sents");
-    socket.emit('chat message', msg);
+    io.emit('chat message', msg);
   });
 });
 
