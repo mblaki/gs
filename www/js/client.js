e7dd451c927240e04856915d11b0b6418ac968ac
Marco de Freitas <19050739@sun.ac.za>
2017-09-22 02:16:23 +0200

    ironing

---
+++ www/js/client.js
@@ -0,0 +1,88 @@
+    var initX = 0;
+    var initY = 0;
+    var canvas = document.getElementById('list');
+    var positionInfo = canvas.getBoundingClientRect();
+    var canvasHeight = positionInfo.height;
+    var canvasWidth = positionInfo.width;
+alert("canvasHeight: " + canvasHeight + " -- canvasWidth: " + canvasWidth);
+        socket.on('game loop', function(){
+            if(gname != "Instructor") {
+                initY += 10;
+                document.getElementById('map').style.backgroundPositionY = initY + "px";
+            }
+        });
+    $(document).keydown(function(e){
+    if(e.keyCode == 38) {//up
+        
+      initY += 10;
+        if(initY >canvasHeight){
+            initY = 0;
+        }
+        if (initX > canvasWidth){
+            initX= 0;
+        }
+        if(initY < -1){
+            initY = canvasHeight;
+        }
+        if (initX < -1){
+            initX= canvasWidth;
+        }
+        document.getElementById('map').style.backgroundPositionY = initY + "px";
+        var e=document.getElementById("list");
+        var c = e.getContext('2d');
+        var p = c.getImageData(initX-50, initY-50, 1, 1).data; 
+        console.log("p0: " + p[0] + " \np1: " + p[1] + " \np2: " + p[2] + " \np3: " + p[3] + "\ninitX: " + initX + "\ninitY: " + initY);
+        if (p[0] < 50 && p[0] != 0){
+            alert("CRASH");
+            console.log("CRASH at "+ initX + " , " + initY);
+        }
+    }
+    else if(e.keyCode == 40) {//down
+      initY -= 10;
+        if(initY > canvasHeight){
+            initY = 0;
+        }
+        if (initX > canvasWidth){
+            initX= 0;
+        }
+        if(initY < -1){
+            initY = canvasHeight;
+        }
+        if (initX < -1){
+            initX= canvasWidth;
+        }
+      document.getElementById('map').style.backgroundPositionY = initY + "px";
+    }
+    else if(e.keyCode == 37) {//left
+      initX += 10;
+        if(initY > canvasHeight){
+            initY = 0;
+        }
+        if (initX > canvasWidth){
+            initX= 0;
+        }
+        if(initY < -1){
+            initY = canvasHeight;
+        }
+        if (initX < -1){
+            initX= canvasWidth;
+        }
+        document.getElementById('map').style.backgroundPositionX = initX + "px";
+    }
+    else if(e.keyCode == 39) {//right
+      initX -= 10;
+        if(initY > canvasHeight){
+            initY = 0;
+        }
+        if (initX > canvasWidth){
+            initX= 0;
+        }
+        if(initY < -1){
+            initY = canvasHeight;
+        }
+        if (initX < -1){
+            initX= canvasWidth;
+        }
+        document.getElementById('map').style.backgroundPositionX = initX + "px";
+    }
+});
---
+++ www/js/messaging.js
@@ -0,0 +1,190 @@
+    var audio = new Audio('../sound/mario1.wav');
+    var gname="";
+    var dest="Instructor";
+    var clientList=[];
+    var c_index=0;
+    var socket = io();
+    var warningList = [];
+    var myWarningList = [];
+    var selected = [];
+    var canvas = document.getElementById('list');
+    var positionInfo = canvas.getBoundingClientRect();
+    var canvasHeight = positionInfo.height;
+    var canvasWidth = positionInfo.width;
+    function changeMap(p){
+        if (p == 0) {
+            $("#map").css("display","block");
+            $("#list").css("display","none");
+        } else {
+            $("#map").css("display","none");
+            $("#list").css("display","block");
+        }
+    }
+        
+      $(function () {
+        $('form').submit(function(){
+          socket.emit('chat message', $('#m').val(), gname, dest);
+          $('#m').val('');
+          return false;
+        });
+        socket.on('chat message', function(msg, name, dest){
+            if (gname == 'Instructor'){
+                console.log(" msg from  "+ name +"appending to tab: " + clientList.indexOf(name) );
+                if(name == "Instructor"){
+                    $("#"+clientList.indexOf(dest)).append($('<p style="float:right;background-color:#ccffcc;">').text(msg));
+                } else {
+                    audio.play();
+                    $("#"+clientList.indexOf(name)).append($('<p style="float:left;background-color:#f2f2f2;">').text(msg));
+                }
+            } else {
+                    if (name == "Instructor"){
+                        audio.play();
+                        $("#0").append($('<p  style="float:left;background-color:#f2f2f2;">').text(msg));
+                    } else {
+                        $("#0").append($('<p style="float:right;background-color:#ccffcc;">').text(msg));
+                        
+                    }
+            }
+        });
+      });
+    $(document).ready( function () {
+    $("#join").click(function(){
+        gname = $("#username").val();
+        $("#prompt").hide();
+        if (gname == "Instructor"){
+            socket.emit('instructor join');
+            $("#users").width("20%");
+            $("#users").css("float","left");
+            $("#map").width("55%");
+            $("#map").css("float","left");
+            $("#chat").width("25%");
+            $("#chat").css("float","right");
+        } else {
+            socket.emit('user join', gname);
+            $("#users").hide();
+            $("#map").width("75%");
+            $("#map").css("float","left");
+            $("#chat").width("25%");
+            $("#chat").css("float","right");
+          //  $(".toggle").css("display","none");
+        }
+    });
+    $("#Abroadcast").click(function(){
+        console.log("all pressed");
+        warningList.length = 0;
+            $('#users :checked').each(function() {
+                warningList.push($(this).val());
+            });
+            socket.emit('all broadcast', warningList);
+    });
+    $("#Sbroadcast").click(function(){
+        warningList.length = 0;
+            $('#users :checked').each(function() {
+                warningList.push($(this).val());
+            });
+            socket.emit('select broadcast', selected, warningList);
+    });
+    });
+        socket.on('instructor join', function(){
+            console.log("instructor join emit");
+            if (gname == "Instructor"){
+                console.log("instructor join emit gname = Instructor");
+                $("#avatar").hide();
+            }
+        });
+        socket.on('user join', function(lname){
+            if(gname == 'Instructor'){
+                clientList[c_index]= lname;
+                console.log("user join clientList " + c_index + " = " + lname);
+                $("#" + c_index).append('<h3 style="clear:both;">'+ lname +'</h3>');
+                $('#buts').append('<button class="tablinks" type = "button" value="'+lname+'" id = "b'+lname+'"  onclick="setDest(event, this.value)">'+lname+'</button>');
+                var element = document.createElement("div");
+                    element.setAttribute("id", lname);
+                    element.innerHTML = lname;
+                    element.onclick = function () {
+                        if ($.inArray(lname, selected) == -1){
+                            selected.push(lname);
+                            console.log(lname + " deselected");
+                            $(this).css("border-style", "solid");
+                            $(this).css("border-width", "2px");
+                            $(this).css("border-color", "white");
+                        } else {
+                            selected.splice($.inArray(lname, selected),1);
+                            console.log(lname + " deselected");
+                            $(this).css("border-style", "none");
+                        }
+                    }
+                    $("#users").append(element);
+                c_index += 1;
+            } else if (gname == lname && gname != "Instructor"){
+                $('#map').append($('<h1> '+lname + 's SCREEN </h1>'));
+                $('#buts').css("display","none");
+                $("#0").css("display","block");
+                 var img = new Image();
+                img.onload = function(){
+                    // create a hidden canvas, exactly the size of our small image
+                  //  var hidden_canvas = document.createElement("canvas");
+                    var hidden_canvas = document.getElementById("list")
+                    hidden_canvas.width = canvasWidth;
+                    hidden_canvas.height = canvasHeight;
+                    var hidden_context = hidden_canvas.getContext("2d");        
+
+                    // draw the image on the hidden canvas
+                    hidden_context.drawImage(img, 0, 0);
+
+                    // set up the visible canvas
+                   // var visible_canvas = document.getElementById("list");
+                 //   var visible_context = visible_canvas.getContext("2d");
+
+                    // draw on the visible canvas using the image from the hidden canvas
+                  //  visible_context.drawImage(hidden_canvas,0,0);    
+                };
+
+                img.src = "../img/dot.jpg";
+               
+            }
+        });
+        socket.on('all broadcast', function(warningList){
+             if (gname != "Instructor"){
+              
+              $('#t').empty();
+              console.log("broadcast to all recieved");
+              myWarningList.length = 0;
+              myWarningList = warningList;
+              myWarningList.forEach(function(value){
+                    $('#t').append($('<p>').text(value));
+               });
+            } else {
+                console.log("instructor igores own all bcast");
+            }
+        });
+        socket.on('select broadcast', function(selected, warningList){
+             if (gname != "Instructor"){
+              $('#t').empty();
+              console.log("broadcast to all recieved");
+              myWarningList.length = 0;
+              myWarningList = warningList;
+              myWarningList.forEach(function(value){
+                    $('#t').append($('<p>').text(value));
+               });
+            } else {
+                console.log("instructor igores own sel bcast");
+            }
+        });
+    function setDest(evt, val){
+         var i, tabcontent, tablinks;
+         this.dest = val;
+         console.log("Tab id=" + val);
+         tabcontent = document.getElementsByClassName("tabcontent");
+         for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+         }
+         tablinks = document.getElementsByClassName("tablinks");
+         for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+         }
+        if (val != null) {
+            document.getElementById(clientList.indexOf(val)).style.display = "block";
+        }
+         evt.currentTarget.className += " active";
+    }
