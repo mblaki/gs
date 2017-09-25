5d1a01da627735f22f1034ae20c457b64f648b80
mblaki <mblaki69@gmail.com>
2017-09-25 18:08:52 +0200

    angles

--- www/js/client.js
+++ www/js/client.js
@@ -7,7 +7,8 @@
     var cy = canvasHeight;
     var increment=0;
     var line_increment = 0;
-    var angle = 0;
+    var angle = 0.0;
+var offset = screen.width *0.55*0.5;
     var l=document.getElementById("list");
     var ctx=l.getContext("2d");
     ctx.beginPath();
@@ -42,23 +43,21 @@
     });
 
     $("#l_but").click(function(){
-        increment +=2;
-        angle-=4.5;
-        if (increment > 10) {
-            increment = 10;
-            angle = -45;
+        if (increment < 10) {
+            increment +=2;
+            angle-=4.5;
+            $("#avatar").rotate(angle);
         }
-        $("#avatar").rotate(angle);
+        
     });
 
     $("#r_but").click(function(){
-        increment -=2;
-        angle += 4.5;
         if (increment < -10) {
-            increment = -10;
-            angle = 45;
+            increment -=2;
+            angle += 4.5;
+            $("#avatar").rotate(angle);
         }
-        $("#avatar").rotate(angle);
+        
     });
 
     $(document).keydown(function(e){
@@ -171,13 +170,13 @@
 function detectCollision(x,y){
     
     //ctx.moveTo(x-60,y-210);
-    ctx.lineTo(x+100,y-200);
+    ctx.lineTo(x+100+offset,y-200);
     ctx.lineWidth = 10;
     ctx.strokeStyle = "rgb(0, 0, 222)";
     ctx.stroke();
     var e=document.getElementById("list");
     var c = e.getContext('2d');
-    var p = c.getImageData(x+110, y-310, 1, 1).data; 
+    var p = c.getImageData(x+110+offset, y-310, 1, 1).data; 
     if (p[0]==62 || p[1]== 117 || p[2] == 198){
         console.log("CRASH "+ "\np0: " + p[0] + " \np1: " + p[1] + " \np2: " + p[2]);
     }
