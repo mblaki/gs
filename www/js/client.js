9a4cec8b4b583823d4679b3a27f987b6a9563d4d
Marco de Freitas <19050739@sun.ac.za>
2017-09-25 22:41:50 +0200

    finish

--- www/js/client.js
+++ www/js/client.js
@@ -182,7 +182,7 @@ function detectCollision(x,y){
   
     var e=document.getElementById("list");
     var c = e.getContext('2d');
-    var p = c.getImageData(x+offset+50, y-400, 1, 1).data;
+    var p = c.getImageData(x+offset+60, y-400, 1, 1).data;
     console.log("\np0: " + p[0] + " \np1: " + p[1] + " \np2: " + p[2]);
     if (p[0]==62 || p[1]== 117 || p[2] == 198){
         console.log("CRASH "+ "\np0: " + p[0] + " \np1: " + p[1] + " \np2: " + p[2]);
