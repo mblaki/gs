cce95388d7ca5a725abaa7fa87d5378788f955c2
Marco de Freitas <19050739@sun.ac.za>
2017-09-25 22:12:08 +0200

    finish

--- www/js/client.js
+++ www/js/client.js
@@ -18,9 +18,9 @@
     socket.on('game loop', function(iX, iY){
         if(gname != "Instructor") {
             initY += iY+increment;
-            cy -= iY+increment;
+            cy =cy - (iY+increment);
             initX += increment;
-            cx -= increment;
+            cx = cx + increment;
             
             if(cy >canvasHeight){
                 alert("TOP BORDER");
