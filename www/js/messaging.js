c051ed2bd819cae09885c1f342cb165b521fbfea
Marco de Freitas <19050739@sun.ac.za>
2017-09-28 12:13:25 +0200

    asd

--- www/js/messaging.js
+++ www/js/messaging.js
@@ -74,7 +74,7 @@
             $("#map").css("float","left");
             $("#chat").width("25%");
             $("#chat").css("float","right");
-            $("#avatar").css("position", "relative");
+            $("#avatar").css("left", screen.width *0.55*0.5);
             $("#avatar").css("visibility", "visible");
             $("#controls").css("left", offset +"px");
             $("#controls").css("visibility", "visible");
