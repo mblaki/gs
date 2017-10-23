7d563ea303053265914049fced6b7ea9eb930163
mblaki <mblaki69@gmail.com>
2017-10-23 17:04:46 +0200

    go for it

--- www/js/messaging.js
+++ www/js/messaging.js
@@ -177,7 +177,7 @@
                 img.onload = function(){
                     var hidden_canvas = document.getElementById("list");
                     hidden_canvas.width = 983;//canvasWidth;
-                    hidden_canvas.height = 772;//canvasHeight;
+                    hidden_canvas.height = 983;//canvasHeight;
                     var hidden_context = hidden_canvas.getContext("2d");        
                     hidden_context.drawImage(img, 0, 0, canvasWidth, canvasHeight, 0, 0, hidden_canvas.width, hidden_canvas.height);
                 };
