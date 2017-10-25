948109d5220fcac6c42c1385657149d0f1426c1c
mblaki <mblaki69@gmail.com>
2017-10-25 17:38:11 +0200

    hahahahaha

--- www/js/client.js
+++ www/js/client.js
@@ -109,7 +109,7 @@ function detectCollision(x,y){
     var e=document.getElementById("list");
     var c = e.getContext('2d');
     var p = c.getImageData((x+offset)*ratio, (y-5)*ratio, 1, 1).data;
-    console.log("\np0: " + p[0] + " \np1: " + p[1] + " \np2: " + p[2]);
+  //  console.log("\np0: " + p[0] + " \np1: " + p[1] + " \np2: " + p[2]);
    if ((p[0]==62 && p[1]== 117 && p[2] == 198) || (p[0]==24 && p[1]== 119 && p[2] == 192)){
         if(dock_ON){
         //    socket.emit('dock', gname, 0);
